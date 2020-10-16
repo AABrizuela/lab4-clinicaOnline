ee265405b1ee49f92a3eb9daa775ae6c44040331
Adrian Brizuela <adrianalebrizuela@gmail.com>
2020-10-16 15:34:05 -0300

    Commit Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import{ MatFormFieldControl } from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatFormFieldControl
   ],
   providers: [],
   bootstrap: [AppComponent]
